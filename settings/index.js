03f01bfcfeacb1ea53e845c4c0fb75adb26535bf
Tyler Fisher <tfisher@npr.org>
2016-08-29 10:52:20 -0400

    setup redis worker

--- settings/index.js
+++ settings/index.js
@@ -21,6 +21,8 @@ var path = require("path");
 module.exports = {
   workingDirectory: path.join(__dirname, "..", "tmp"),
   storagePath: path.join(__dirname, "..", "media"),
+  redisHost: "127.0.0.1",
+  worker: true,
   fonts: [
     { family: "Gotham", file: path.join(__dirname, "fonts", "Gotham-Book.ttf") },
     { family: "Gotham", file: path.join(__dirname, "fonts", "Gotham-Light.ttf"), weight: 300 },
