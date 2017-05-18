50ca5d5c37447c36ecc3f500adb1b0b5428f4af4
Phillip Shannon <shannonphillipl@gmail.com>
2017-05-18 12:13:45 -0400

    Add UI for timed captions

---
+++ editor/js/timed-captions.js
@@ -0,0 +1,17 @@
+function addAnotherCaption() {
+  $("#captions").append("<div class='specific-caption'><div class='col-60 caption-text'> <input class='input-caption-text' name='caption' type='text' autocomplete='off' placeholder='Enter caption' /> </div><div class='col-20 caption-time'> <input class='input-caption-time' name='time' type='text' autocomplete='off' placeholder='0' /></div><div class='col-20'><div class='delete-caption'><h5>X<h5></div></div></div>");
+}
+
+function removeCaption(div) {
+  div.parentElement.parentElement.remove();
+}
+$(document).ready(function() {
+  $("#add-more-captions").click(function() {
+    $(".hide-until-captions-added").show();
+    addAnotherCaption();
+  });
+
+  $('body').on('click', '.delete-caption', function() {
+    removeCaption(this);
+  });
+});
